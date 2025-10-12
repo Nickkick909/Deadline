3a04b6a39cf2e2b9027465db5451337dcf990aa0
Nicholas Knight <nicholasknight1999@yahoo.com>
2025-10-12 19:26:56 -0400

    Updating logic for chase state to include player detection

---
+++ Assets/Scripts/ChaseState.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseState: State
+{
+    float timeBeforeSleep;
+
+    protected override void OnEnter()
+    {
+        // "What was that!?"
+        timeBeforeSleep = 5;
+
+        Debug.Log("Chase State");
+
+        //RandomIdleAnimation();
+
+        sc.agent.SetDestination(sc.player.transform.position);
+        sc.RandomWalkAnimation();
+    }
+
+    protected override void OnUpdate()
+    {
+        if (sc.DetectPlayer(1f))
+        {
+            sc.ChangeState(sc.attackState);
+            return;
+        }
+
+        if (!sc.agent.pathPending)
+        {
+            if (sc.agent.remainingDistance <= sc.agent.stoppingDistance)
+            {
+                if (!sc.agent.hasPath || sc.agent.velocity.sqrMagnitude == 0f)
+                {
+                    if (sc.DetectPlayer())
+                    {
+                        sc.ChangeState(sc.chaseState);
+                        return;
+                    }
+
+                    sc.ChangeState(sc.idleState);
+                }
+            }
+        }
+    }
+
+
+    protected override void OnHurt()
+    {
+        // Transition to Hurt State
+        //sc.ChangeState(sc.hurtState);
+
+    }
+    protected override void OnExit()
+    {
+        // "Must've been the wind"
+    }
+}
